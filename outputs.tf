fb07f86731401d5d2a74fbc91c91ebe5978dfd9c
glavk <sea.groundwork@gmail.com>
2020-11-28 01:28:47 +0500

    simplify names for outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "compute_instance_internal_ip" {
+output "internal_ip" {
   description = "The internal IP address of the instance"
   value       = yandex_compute_instance.this.network_interface.0.ip_address
 }
 
-output "compute_instance_external_ip" {
+output "external_ip" {
   description = "The external IP address of the instance"
   value       = yandex_compute_instance.this.network_interface.0.nat_ip_address
 }
 
-output "compute_instance_fqdn" {
+output "fqdn" {
   description = "The fully qualified DNS name of this instance"
   value       = yandex_compute_instance.this.fqdn
 }
