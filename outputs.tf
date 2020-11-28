8c31364dd5c0fbe8e7a23290e39d4275426d9280
glavk <sea.groundwork@gmail.com>
2020-11-28 23:16:14 +0500

    fix output with index

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "internal_ip" {
   description = "The internal IP address of the instance"
-  value       = yandex_compute_instance.this.network_interface.0.ip_address
+  value       = yandex_compute_instance.this[count.index].network_interface.0.ip_address
 }
 
 output "external_ip" {
   description = "The external IP address of the instance"
-  value       = yandex_compute_instance.this.network_interface.0.nat_ip_address
+  value       = yandex_compute_instance.this[count.index].network_interface.0.nat_ip_address
 }
 
 output "fqdn" {
   description = "The fully qualified DNS name of this instance"
-  value       = yandex_compute_instance.this.fqdn
+  value       = yandex_compute_instance.this[count.index].fqdn
 }
